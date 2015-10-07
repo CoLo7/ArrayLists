9690c5e069bdaacbfe216e55fe8821828da1f5f3
CoLo7 <gabeaguilar7@gmail.com>
2015-10-07 09:55:07 -0600

    added showList method

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,10 +1,19 @@
 package lists.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
+		firstWords.add("lkajdfaodijfa");
 		
 	}
 	
